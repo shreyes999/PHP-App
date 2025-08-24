00c3678d29abf3429ab07814765a492ab5bc89a1
shreyes999 <shreyesr9823@gmail.com>
2025-08-24 18:10:17 +0100

    modified

--- controller/note-create.php
+++ controller/note-create.php
@@ -6,10 +6,19 @@ $db = new Database($config['database']);
 $heading = "Create Notes";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query("INSERT INTO `tables`.`notes` (`body`, `user_id`) VALUES (:body, :user_id) ", [
-        'body' => $_POST['body'],
-        'user_id' => 7
-    ]);
+    $error = [];
+    if (strlen($_POST['body']) == 0) {
+        $error['body'] = 'A body is required';
+    }
+    if (strlen($_POST['body']) > 1000) {
+        $error['body'] = 'The body can not be more than 1,000 characters';
+    }
+    if (empty($error)) {
+        $db->query("INSERT INTO `tables`.`notes` (`body`, `user_id`) VALUES (:body, :user_id) ", [
+            'body' => $_POST['body'],
+            'user_id' => 7
+        ]);
+    }
 }
 
 require "views/note-create.view.php";
